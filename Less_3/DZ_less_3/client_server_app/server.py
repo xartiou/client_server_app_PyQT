7328ee2f84bd40cf999dbd7cb24c1ea63684bef3
xartiou <xartiou@gmail.com>
2022-04-12 18:22:38 +0300

    Less_3. DZ_less_3

--- Less_3/DZ_less_3/client_server_app/server.py
+++ Less_3/DZ_less_3/client_server_app/server.py
@@ -1,10 +1,19 @@
-import argparse
-import select
 import socket
-
+import sys
+import argparse
+import json
+import logging
+import select
+import time
+import logs.server_log_config
+from errors import IncorrectDataRecivedError
+from common.variables import *
 from common.utils import *
 from decorators import log
+from descryptors import Port
 from metaclasses import ServerVerifier
+import threading
+from server_database import ServerDB
 
 # Инициализация логирования сервера.
 logger = logging.getLogger('server_dist')
@@ -23,11 +32,16 @@ def arg_parser():
 
 
 # Основной класс сервера
-class Server(metaclass=ServerVerifier):
-    def __init__(self, listen_address, listen_port):
+class Server(threading.Thread, metaclass=ServerVerifier):
+    port = Port()
+
+    def __init__(self, listen_address, listen_port, database):
         # Параметры подключения
         self.addr = listen_address
         self.port = listen_port
+
+        # База данных сервера
+        self.database = database
 
         # Список подключённых клиентов.
         self.clients = []
@@ -38,6 +52,9 @@ class Server(metaclass=ServerVerifier):
         # Словарь содержащий сопоставленные имена и соответствующие им сокеты.
         self.names = dict()
 
+        # Конструктор предка
+        super().__init__()
+
     def init_socket(self):
         logger.info(
             f'Запущен сервер, порт для подключений: {self.port}, '
@@ -45,6 +62,7 @@ class Server(metaclass=ServerVerifier):
             f'Если адрес не указан, принимаются соединения с любых адресов.')
         # Готовим сокет
         transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        transport.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         transport.bind((self.addr, self.port))
         transport.settimeout(0.5)
 
@@ -52,7 +70,7 @@ class Server(metaclass=ServerVerifier):
         self.sock = transport
         self.sock.listen()
 
-    def main_loop(self):
+    def run(self):
         # Инициализация Сокета
         self.init_socket()
 
@@ -64,7 +82,7 @@ class Server(metaclass=ServerVerifier):
             except OSError:
                 pass
             else:
-                logger.info(f'Установлено соединение с ПК {client_address}')
+                logger.info(f'Установлено соедение с ПК {client_address}')
                 self.clients.append(client)
 
             recv_data_lst = []
@@ -126,6 +144,8 @@ class Server(metaclass=ServerVerifier):
             # иначе отправляем ответ и завершаем соединение.
             if message[USER][ACCOUNT_NAME] not in self.names.keys():
                 self.names[message[USER][ACCOUNT_NAME]] = client
+                client_ip, client_port = client.getpeername()
+                self.database.user_login(message[USER][ACCOUNT_NAME], client_ip, client_port)
                 send_message(client, RESPONSE_200)
             else:
                 response = RESPONSE_400
@@ -147,6 +167,7 @@ class Server(metaclass=ServerVerifier):
         elif ACTION in message \
                 and message[ACTION] == EXIT \
                 and ACCOUNT_NAME in message:
+            self.database.user_logout(message[ACCOUNT_NAME])
             self.clients.remove(self.names[ACCOUNT_NAME])
             self.names[ACCOUNT_NAME].close()
             del self.names[ACCOUNT_NAME]
@@ -159,15 +180,51 @@ class Server(metaclass=ServerVerifier):
             return
 
 
+def print_help():
+    print('Поддерживаемые комманды:')
+    print('users - список известных пользователей')
+    print('connected - список подключённых пользователей')
+    print('loghist - история входов пользователя')
+    print('exit - завершение работы сервера.')
+    print('help - вывод справки по поддерживаемым командам')
+
+
 def main():
     # Загрузка параметров командной строки, если нет параметров,
     # то задаём значения по умолчанию.
     listen_address, listen_port = arg_parser()
 
-    # Создание экземпляра класса - сервера.
-    server = Server(listen_address, listen_port)
-    server.main_loop()
+    # Инициализация базы данных
+    database = ServerDB()
 
+    # Создание экземпляра класса - сервера.
+    server = Server(listen_address, listen_port, database)
+    server.daemon = True
+    server.start()
+
+    # Печатаем справку:
+    print_help()
+
+    # Основной цикл сервера:
+    while True:
+        command = input('Введите команду: ')
+        if command == 'help':
+            print_help()
+        elif command == 'exit':
+            break
+        elif command == 'users':
+            for user in sorted(database.users_list()):
+                print(f'Пользователь {user[0]}, последний вход: {user[1]}')
+        elif command == 'connected':
+            for user in sorted(database.active_users_list()):
+                print(f'Пользователь {user[0]}, подключен: {user[1]}:{user[2]}, время установки соединения: {user[3]}')
+        elif command == 'loghist':
+            name = input('Введите имя пользователя для просмотра истории. '
+                         'Для вывода всей истории, просто нажмите Enter: ')
+            for user in sorted(database.login_history(name)):
+                print(f'Пользователь: {user[0]} время входа: {user[1]}. Вход с: {user[2]}:{user[3]}')
+        else:
+            print('Команда не распознана.')
 
 if __name__ == '__main__':
     main()
--- Less_3/DZ_less_3/client_server_app/server_database.py
+++ Less_3/DZ_less_3/client_server_app/server_database.py
@@ -1,188 +1,156 @@
+from sqlalchemy import create_engine, Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 import datetime
-from sqlite3 import  *
-from sqlalchemy import create_engine, Table, Column, MetaData, ForeignKey, DateTime, Integer, String
-from sqlalchemy.orm import mapper, sessionmaker
-from common.variables import *
 
-# Класс - серверная база данных
-class ServerStorage:
-    # Класс - отображение таблицы всех пользователей
-        # Экземпляр этого класса - запись в таблице AllUsers
-    class AllUsers:
-        def __init__(self, username):
-            self.name = username
-            self.last_login = datetime.datetime.now()
-            self.id = None
 
-    # Класс - отображение таблицы активных пользователей:
-        # Экземпляр этого класса - запись в таблице ActiveUsers
-    class ActiveUsers:
-        def __init__(self, user_id, ip_address, port, login_time):
-            self.user = user_id
-            self.ip_address = ip_address
-            self.port = port
-            self.login_time = login_time
-            self.id = None
+class ServerDB:
+    Base = declarative_base()
 
-    # Класс - отображение таблицы истории входов:
-        # Экземпляр этого класса - запись в таблице LoginHistory
-    class LoginHistory:
-        def __init__(self, name, date, ip, port):
-            self.id = None
-            self.name = name
-            self.date_time = date
+    class AllUsers(Base):
+        __tablename__ = 'all_users'
+        id = Column(Integer, primary_key=True)
+        login = Column(String, unique=True)
+        last_conn = Column(DateTime)
+
+        def __init__(self, login):
+            self.login = login
+            self.last_conn = datetime.datetime.now()
+
+    class ActiveUsers(Base):
+        __tablename__ = 'active_users'
+        id = Column(Integer, primary_key=True)
+        user = Column(String, ForeignKey('all_users.id'), unique=True)
+        ip = Column(String)
+        port = Column(Integer)
+        time_conn = Column(DateTime)
+
+        def __init__(self, user, ip, port, time_conn):
+            self.user = user
             self.ip = ip
             self.port = port
+            self.time_conn = time_conn
+
+    class LoginHistory(Base):
+        __tablename__ = 'login_history'
+        id = Column(Integer, primary_key=True)
+        user = Column(String, ForeignKey('all_users.id'))
+        ip = Column(String)
+        port = Column(Integer)
+        last_conn = Column(DateTime)
+
+        def __init__(self, user, ip, port, last_conn):
+            self.user = user
+            self.ip = ip
+            self.port = port
+            self.last_conn = last_conn
 
     def __init__(self):
-        # Создаем движок БД
-        # SERVER_DATABASE - sqlite:///server_base.db3
-        # echo=False - отключает вывод на экран sql-запросов
-        # pool_recycle - по умолчанию соединение с БД через 8 часов простоя обрывается,
-        # чтобы этого не произошло необходимо pool_recycle=7200(пересоединение через каждые 2 часа)
-        self.database_engine = create_engine(SERVER_DATABASE, echo=False, pool_recycle=7200)
+        self.engine = create_engine('sqlite:///server_base.db3', echo=False, pool_recycle=7200)
 
-        # Создаем объект MetaData
-        self.metadata = MetaData()
-
-        # Создаем таблицу пользователей
-        user_table = Table('Users', self.metadata,
-                           Column('id', Integer, primary_key=True),
-                           Column('name', String, unique=True),
-                           Column('last_login', DateTime),
-                           )
-
-        # Создаем таблицу активных пользователей
-        active_users_table = Table('Active_users', self.metadata,
-                                   Column('id', Integer, primary_key=True),
-                                   Column('user', ForeignKey('Users.id'), unique=True),
-                                   Column('ip_address', String),
-                                   Column('port', Integer),
-                                   Column('login_time', DateTime)
-                                   )
-
-        # Создаем таблицу истории входов
-        user_login_history = Table('Login_history', self.metadata,
-                                   Column('id', Integer, primary_key=True),
-                                   Column('name', ForeignKey('Users.id')),
-                                   Column('date_time', DateTime),
-                                   Column('ip', String),
-                                   Column('port', String)
-                                   )
-
-        # Создаем таблицы
-        self.metadata.create_all(self.database_engine)
-
-        # Создаем отображения
-        # Связываем класс в ORM с таблицей
-        mapper(self.AllUsers, user_table)
-        mapper(self.ActiveUsers, active_users_table)
-        mapper(self.LoginHistory, user_login_history)
-
-        # Создаем сессию
-        Session = sessionmaker(bin=self.database_engine)
+        self.Base.metadata.create_all(self.engine)
+        Session = sessionmaker(bind=self.engine)
         self.session = Session()
 
-        # Если в таблице активных юзеров есть записи, то их нужно удалить
-        # Когда устанавливаем соединение, очищаем таблицу активных юзеров
+        # Если в таблице активных пользователей есть записи, то их необходимо удалить
+        # Когда устанавливаем соединение, очищаем таблицу активных пользователей
         self.session.query(self.ActiveUsers).delete()
         self.session.commit()
 
-    # Функция выполняющаяся при входе пользователя записывает в базу факт входа
+    # Функция выполняется при входе пользователя, фиксирует в базе сам факт входа
     def user_login(self, username, ip_address, port):
-        print(username, ip_address, port)
-        # запрос в таблицу пользователей на наличие пользователей с таким же именем
-        rez = self.session.query(self.AllUsers).filter_by(name=username)
-        # Если такое имя пользователя уже присутствует в табл, обновляем время последнего входа
+        # Запрос в таблицу пользователей на наличие там пользователя с таким именем
+        rez = self.session.query(self.AllUsers).filter_by(login=username)
+        # print(type(rez))
+        # Если имя пользователя уже присутствует в таблице, обновляем время последнего входа
         if rez.count():
             user = rez.first()
-            user.last_login = datetime.datetime.now()
-        # иначе создаем нового пользователя
+            user.last_conn = datetime.datetime.now()
+        # Если нет, то создаём нового пользователя
         else:
-            # Создаем экземпляр класса self.AllUsers, через который передаем данные в табл
+            # Создаем экземпляр класса self.AllUsers, через который передаем данные в таблицу
             user = self.AllUsers(username)
             self.session.add(user)
-            # Чтобы создать нового юзера, id которого будет использовано для добавления в табл active_users_table
+            # Коммит здесь нужен, чтобы в db записался ID
             self.session.commit()
 
-        # теперь можно создать запись в active_users_table о факте входа.
-        # создаем экземпляр класса self.ActiveUsers, через него и передадим данные в табл
-        new_active_user = self.ActiveUsers(user.id, datetime.datetime.now(), ip_address, port)
+        # Теперь можно создать запись в таблицу активных пользователей о факте входа.
+        # Создаем экземпляр класса self.ActiveUsers, через который передаем данные в таблицу
+        new_active_user = self.ActiveUsers(user.id, ip_address, port, datetime.datetime.now())
         self.session.add(new_active_user)
 
-        # # создаем экземпляр класса self.LoginHistory, через него и передадим данные в табл
-        history = self.LoginHistory(user.id, datetime.datetime.now(),ip_address, port)
+        # и сохранить в историю входов
+        # Создаем экземпляр класса self.LoginHistory, через который передаем данные в таблицу
+        history = self.LoginHistory(user.id, ip_address, port, datetime.datetime.now())
         self.session.add(history)
 
-        # обязательно сохраняем изменения
+        # Сохраняем изменения
         self.session.commit()
 
-    # Функция, которая фиксирует отключения пользователя
+    # Функция фиксирует отключение пользователя
     def user_logout(self, username):
-        # запрашиваем покидающего пользователя из таблицы self.AllUsers
-        user = self.session.query(self.AllUsers).filter_by(name=username).first()
-        # удаляем его из таблицы self.ActiveUsers
-        self.session.query(self.AllUsers).filter_by(user=user.id).delete()
-        # commit
+        # Запрашиваем пользователя, что покидает нас
+        # получаем запись из таблицы AllUsers
+        user = self.session.query(self.AllUsers).filter_by(login=username).first()
+
+        # Удаляем его из таблицы активных пользователей.
+        # Удаляем запись из таблицы ActiveUsers
+        self.session.query(self.ActiveUsers).filter_by(user=user.id).delete()
+
+        # Применяем изменения
         self.session.commit()
 
-    # Функция, которая возвращает список известных пользователей со временем их последнего входа
+    # Функция возвращает список известных пользователей со временем последнего входа.
     def users_list(self):
-        # Запрос строк таблицы пользователей.
         query = self.session.query(
-            self.AllUsers.name,
-            self.AllUsers.last_login
+            self.AllUsers.login,
+            self.AllUsers.last_conn,
         )
-    #     Возвращаем список кортежей
+        # Возвращаем список тюплов
         return query.all()
 
     # Функция возвращает список активных пользователей
     def active_users_list(self):
-        # Запрашиваем соединение таблиц и собираем кортежи имя, адрес, порт, время.
+        # Запрашиваем соединение таблиц и собираем тюплы имя, адрес, порт, время.
         query = self.session.query(
-            self.AllUsers.name,
-            self.ActiveUsers.ip_address,
+            self.AllUsers.login,
+            self.ActiveUsers.ip,
             self.ActiveUsers.port,
-            self.ActiveUsers.login_time
-        ).join(self.AllUsers)
-        # Возвращаем список кортежей
+            self.ActiveUsers.time_conn
+            ).join(self.AllUsers)
+        # Возвращаем список тюплов
         return query.all()
 
-    # Функция, возвращающая историю входов по пользователю или всем пользователям
+    # Функция возвращает историю входов по пользователю или по всем пользователям
     def login_history(self, username=None):
         # Запрашиваем историю входа
-        query = self.session.query(self.AllUsers.name,
-                                   self.LoginHistory.date_time,
+        query = self.session.query(self.AllUsers.login,
+                                   self.LoginHistory.last_conn,
                                    self.LoginHistory.ip,
                                    self.LoginHistory.port
                                    ).join(self.AllUsers)
-        # Если было указано имя пользователя, то фильтруем по этому имени
+        # Если было указано имя пользователя, то фильтруем по нему
         if username:
-            query = query.filter(self.AllUsers.name == username)
-        # Возвращаем список кортежей
+            query = query.filter(self.AllUsers.login == username)
         return query.all()
 
-# Обязательная отладка
+
 if __name__ == '__main__':
-    test_db = ServerStorage()
-    # Выполняем "подключение" пользователя
-    test_db.user_login('client_1', '192.168.1.4', 8080)
-    test_db.user_login('client_2', '192.168.1.5', 7777)
+    db = ServerDB()
+    db.user_login('client_1', '192.168.1.4', 8888)
+    db.user_login('client_2', '192.168.1.5', 7777)
+    # выводим список кортежей - активных пользователей
+    print(db.active_users_list())
+    # выполянем 'отключение' пользователя
+    db.user_logout('client_1')
+    print(db.users_list())
+    # выводим список активных пользователей
+    print(db.active_users_list())
+    db.user_logout('client_2')
+    print(db.users_list())
+    print(db.active_users_list())
 
-    # Выводим список кортежей - активных пользователей
-    print(' ---- test_db.active_users_list() ----')
-    print(test_db.active_users_list())
-
-    # Выполняем "отключение" пользователя
-    test_db.user_logout('client_1')
-    # И выводим список активных пользователей
-    print(' ---- test_db.active_users_list() after logout client_1 ----')
-    print(test_db.active_users_list())
-
-    # Запрашиваем историю входов по пользователю
-    print(' ---- test_db.login_history(client_1) ----')
-    print(test_db.login_history('client_1'))
-
-    # и выводим список известных пользователей
-    print(' ---- test_db.users_list() ----')
-    print(test_db.users_list())
+    # запрашиваем историю входов по пользователю
+    # db.login_history('client_1')
+    # # выводим список известных пользователей
+    # print(db.users_list())
