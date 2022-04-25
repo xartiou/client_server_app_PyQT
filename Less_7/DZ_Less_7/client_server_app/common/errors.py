297863c940811a52dd3a1835cd184e1cfcd72388
xartiou <xartiou@gmail.com>
2022-04-25 18:10:52 +0300

    create Less_7

---
+++ Less_7/DZ_Less_7/client_server_app/common/errors.py
@@ -0,0 +1,16 @@
+"""Ошибки"""
+
+
+class ServerError(Exception):
+    """
+    Класс - исключение, для обработки ошибок сервера.
+    При генерации требует строку с описанием ошибки,
+    полученную с сервера.
+    """
+
+    def __init__(self, text):
+        self.text = text
+
+    def __str__(self):
+        return self.text
+    
