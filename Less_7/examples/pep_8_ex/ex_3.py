6c65b0cb7ea47eb7cfbe2a765bcba6e5cfddb45b
xartiou <xartiou@gmail.com>
2022-04-25 18:14:16 +0300

    PeP-8, Sphinx Less_7

---
+++ Less_7/examples/pep_8_ex/ex_3.py
@@ -0,0 +1,35 @@
+# правильно
+spam(ham[1], {eggs: 2})
+
+# неправильно
+spam( ham[ 1 ], { eggs: 2 } )
+
+# правильно
+if x == 4:
+    x, y = y, x
+
+# неправильно
+if x == 4 :
+    x , y = y , x
+
+# правильно
+spam(1)
+
+# неправильно
+spam (1)
+
+# правильно
+dict['key'] = list[index]
+
+# неправильно
+dict ['key'] = list [index]
+
+# правильно
+x = 1
+y = 2
+long_variable = 3
+
+# неправильно
+x             = 1
+y             = 2
+long_variable = 3
